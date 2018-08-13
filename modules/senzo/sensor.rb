98a14154368dc7cbcb89c117f08b82d76d52cb0f
pkheav <philip.kheav@hotmail.com>
2018-08-13 15:33:28 +1000

    fixes

--- modules/senzo/sensor.rb
+++ modules/senzo/sensor.rb
@@ -17,10 +17,10 @@ class Senzo::Sensor
     # https://backend.senzodata.com/api/user/me?apikey=363497c3-7089-433a-9b20-671f57088cb7
 
     # node stucture
-    # https://backend.senzodata.com/api/node/structure/4923?apikey=363497c3-7089-433a-9b20-671f57088cb7
+    # https://backend.senzodata.com/api/node/structure/4991?apikey=363497c3-7089-433a-9b20-671f57088cb7
 
     # live sensor status
-    # https://backend.senzodata.com/api/sensor/live/4923?apikey=363497c3-7089-433a-9b20-671f57088cb7
+    # https://backend.senzodata.com/api/sensor/live/4991?apikey=363497c3-7089-433a-9b20-671f57088cb7
     default_settings({
         api_key: '363497c3-7089-433a-9b20-671f57088cb7',
     })
@@ -31,23 +31,14 @@ class Senzo::Sensor
 
     def on_update
         stop
-        @url = "http://backend.senzodata.com/api/senzor/live/4923?apikey=#{setting(:api_key)}"
-        logger.debug { "url is #{@url}" }
+        @url = "https://backend.senzodata.com/api/sensor/live/4923?apikey=#{setting(:api_key)}"
     end
 
     # Return true when detected and false when not
     def in_use?
-        ret = Net::HTTP.get(URI.parse(@url)) # get sensor information in JSON format from api
+        logger.debug { @url }
+        ret = Net::HTTP.get(URI.parse(@url))
         parsed = JSON.parse(ret) # parse the JSON string into a usable hash table
-        logger.debug { "#{parsed.keys}" }
-        presence = parsed['inuse']
-    end
-
-    # check for all bookings now and schedule the check for every future 7am
-    def start
-
-    end
-
-    def stop
+        presence = parsed[0]['inuse']
     end
 end
